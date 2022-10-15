4614f7b178dea1c7ac5eef865da9ca2b84b18029
Hezzy1 <eassyeni88@gmail.com>
2022-10-15 14:02:33 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  *Description: main - This Prints out a string.
+  *Return: 0 if successful.
+  */
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
