55e139c1c6115256fb1a25632184068f1b2c65b5
Hezzy1 <eassyeni88@gmail.com>
2022-10-17 01:44:13 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,10 +7,12 @@
 	* main-program entry point.
 	* Return:0-no error,non zero value if error.
 	**/
-int main(void) 
+int main(void)
 {
 	int n;
+
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
 	if (n >  0)
 	{
