669044e627017afc68dd82090380602dbab36273
Hezzy1 <eassyeni88@gmail.com>
2022-10-19 03:25:39 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  *Description: main- prints _putchar
+  *Return: 0(successful)
+  */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putch(0);
+	return (0);
+}
