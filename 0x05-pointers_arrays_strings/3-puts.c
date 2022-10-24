15dde347b8e33360a2408d33f30909e8e36cd782
Hezzy1 <eassyeni88@gmail.com>
2022-10-24 10:09:52 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+ #include "main.h"
+
+/**
+  * _put - prints a string
+  * @str: string to be printed
+  */
+
+void _puts(char *str)
+{
+
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+
+}
